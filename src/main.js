6e29bcff2eecfbf85f0e737cf58ca0682174d717
Aleksey Kurkov <aleksey.kurkov@gmail.com>
2017-06-13 16:07:55 +0300

    Directive "C" - class

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ var app = angular.module("angularBlackbox", []);
 
 app.directive("mydirective", function () {
     return {
-        restrict: "A",
+        restrict: "C",
         link: function () {
             alert("It works!!!");
         }
