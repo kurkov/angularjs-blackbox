fac20778a37a026a2997dff7efb8f987e6f0f9ae
Aleksey Kurkov <aleksey.kurkov@gmail.com>
2017-06-13 16:33:21 +0300

    Combination of 2 attribute directives for 1 div element

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
 var app = angular.module("angularBlackbox", []);
 
-app.directive("mydirective", function () {
+app.directive("mydirective1", function () {
     return {
-        restrict: "M",
+        restrict: "A",
+        priority: 100,
         link: function () {
-            alert("It works!!!");
+            alert("mydirective1 works!!!");
+        }
+    }
+});
+
+app.directive("mydirective2", function () {
+    return {
+        restrict: "A",
+        priority: 200,
+        link: function () {
+            alert("mydirective2 works!!!");
         }
     }
 });
