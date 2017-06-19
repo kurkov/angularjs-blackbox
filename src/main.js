370df3a408382f4703711594381bb32c07c29347
Aleksey Kurkov <aleksey.kurkov@gmail.com>
2017-06-19 19:53:34 +0300

    Directive "highlight" with parameter

--- src/main.js
+++ src/main.js
@@ -3,12 +3,12 @@ var app = angular.module("angularBlackbox", []);
 app.directive("highlight", function () {
     return {
         restrict: "A",
-        link: function (scope, element) {
+        link: function (scope, element, attrs) {
             element.bind("mouseenter", function () {
-                element.addClass("alert-info");
+                element.addClass(attrs.highlight);
             });
             element.bind("mouseleave", function () {
-                element.removeClass("alert-info");
+                element.removeClass(attrs.highlight);
             });
         }
     }
